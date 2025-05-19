f9caaa1354cb162b51b99da62ea73c0462a31d07
devesh010704 <devesh10504@gmail.com>
2025-05-20 00:02:03 +0530

    cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloud } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  if (!localFilePath) return null;
+  try {
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
